169646a37dc1d49bd1f827f6d7a2eaaa94557d9a
Chaimaâ AFKIR <112092106+Mileva000@users.noreply.github.com>
2024-12-05 18:21:39 +0100

    npm run build

--- clientside/vite.config.js
+++ clientside/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../public/build", // Output build files to Laravel's public/react directory
+    outDir: "build", // Output build files to Laravel's public/react directory
     emptyOutDir: true, // Clear the output directory before building
   },
   server: {
